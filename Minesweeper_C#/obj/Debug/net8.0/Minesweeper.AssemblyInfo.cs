aadf7a5034f30653ab6258d19e1e2dedf8768b03
Viorel Tsigos <viorel.tsigos@gmail.com>
2024-04-05 17:20:08 +0200

    Update assembly version in Minesweeper_C# project

--- Minesweeper_C#/obj/Debug/net8.0/Minesweeper.AssemblyInfo.cs
+++ Minesweeper_C#/obj/Debug/net8.0/Minesweeper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29e744eb09cea392739d9bb3443487a847dbab15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faf4146de090fa51a4ff0acacf616b14434e0d5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
